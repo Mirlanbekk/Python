ed224ff22ca19ba9b2bffd08b0bca0ab8277b4eb
Mirlanbekk <pirmatov@gmail.com>
2022-09-22 08:11:17 -0700

    create reverse list

--- python/reverse.py
+++ python/reverse.py
@@ -5,7 +5,9 @@ a = input()
 l = len(a)  # length of a string.
 k = []
 
-for i in range(l-1,-1,-1):  
-    k.append(a[i])
+for i in range(l-1,-1,-1):  #1st element starting point. 2sd ending element. 3th increment or decrement.
+    k.append(a[i])  
 r = "".join(k)   # convert list elements to a single string.
 print(r)    
+
+
